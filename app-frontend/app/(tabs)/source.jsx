c93cbe7f9a7d90355bd20c0dfac43e6a5c1cb99f
236sec <popole159za@gmail.com>
2024-08-09 15:28:55 +0700

    Add Quiz Card and Quiz detailed Page

--- app-frontend/app/(tabs)/source.jsx
+++ app-frontend/app/(tabs)/source.jsx
@@ -2,23 +2,62 @@ import { View, Text, FlatList } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SourceCard from "../../components/SourceCard";
+import QuizCard from "../../components/QuizCard";
 
 const DATA = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
     title: "First Item",
+    type: "quiz",
   },
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
     title: "Second Item",
+    type: "source",
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d72",
     title: "Third Item",
+    type: "source",
+  },
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+    type: "quiz",
+  },
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+    type: "quiz",
+  },
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+    type: "quiz",
+  },
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+    type: "quiz",
+  },
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+    type: "quiz",
+  },
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+    type: "quiz",
   },
 ];
 
-const Item = ({ title }) => <SourceCard />;
+const Item = ({ title, type }) => {
+  if (type == "source") {
+    return <SourceCard title={title} />;
+  }
+  return <QuizCard title={title} />;
+};
 
 export default function Source() {
   return (
@@ -26,7 +65,7 @@ export default function Source() {
       <Text className="text-3xl">Source Page</Text>
       <FlatList
         data={DATA}
-        renderItem={({ item }) => <Item title={item.title} />}
+        renderItem={({ item }) => <Item title={item.title} type={item.type} />}
         keyExtractor={(item) => item.id}
         className="w-10/12"
       />
--- app-frontend/constants/images.js
+++ app-frontend/constants/images.js
@@ -1,3 +1,6 @@
 import book from "../assets/images/book.svg";
+import quizpaper from "../assets/images/quizpaper.svg";
+import startbutton from "../assets/images/startbutton.svg";
+import light from "../assets/images/light.svg";
 
-export default { book };
+export default { book, quizpaper, startbutton, light };
