b6467bef136870522f58f7b0101b3487ae27d8e9
236sec <popole159za@gmail.com>
2024-08-22 16:03:09 +0700

    npm lint

--- app-backend/src/app.module.ts
+++ app-backend/src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TagsModule } from './tags/tags.module';
+import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
   imports: [
