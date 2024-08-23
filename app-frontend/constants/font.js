5e1e8e10500945bcf50e38f9822157127c35f269
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-08-23 10:58:45 +0700

    add font

---
+++ app-frontend/constants/font.js
@@ -0,0 +1,297 @@
+const fonts = {
+    EngLight8: {
+      fontFamily: "InterLight",
+      fontSize: 8
+    },
+    EngRegular8: {
+        fontFamily: "InterRegular",
+        fontSize: 8
+      },
+    EngMedium8: {
+        fontFamily: "InterMedium",
+        fontSize: 8
+    },
+    EngSemiBold8: {
+        fontFamily: "InterSemiBold",
+        fontSize: 8
+    },
+    EngBold8: {
+        fontFamily: "InterBold",
+        fontSize: 8
+    },
+    EngLight10: {
+        fontFamily: "InterLight",
+        fontSize: 10
+      },
+      EngRegular10: {
+          fontFamily: "InterRegular",
+          fontSize: 10
+        },
+      EngMedium10: {
+          fontFamily: "InterMedium",
+          fontSize: 10
+      },
+      EngSemiBold10: {
+          fontFamily: "InterSemiBold",
+          fontSize: 10
+      },
+      EngBold10: {
+          fontFamily: "InterBold",
+          fontSize: 10
+      },
+
+      EngLight12: {
+        fontFamily: "InterLight",
+        fontSize: 12
+      },
+      EngRegular12: {
+          fontFamily: "InterRegular",
+          fontSize: 12
+        },
+      EngMedium12: {
+          fontFamily: "InterMedium",
+          fontSize: 12
+      },
+      EngSemiBold12: {
+          fontFamily: "InterSemiBold",
+          fontSize: 12
+      },
+      EngBold12: {
+          fontFamily: "InterBold",
+          fontSize: 12
+      },
+
+      EngLight14: {
+        fontFamily: "InterLight",
+        fontSize: 14
+      },
+      EngRegular14: {
+          fontFamily: "InterRegular",
+          fontSize: 14
+        },
+      EngMedium14: {
+          fontFamily: "InterMedium",
+          fontSize: 14
+      },
+      EngSemiBold14: {
+          fontFamily: "InterSemiBold",
+          fontSize: 14
+      },
+      EngBold14: {
+          fontFamily: "InterBold",
+          fontSize: 14
+      },
+
+      EngLight16: {
+        fontFamily: "InterLight",
+        fontSize: 16
+      },
+      EngRegular16: {
+          fontFamily: "InterRegular",
+          fontSize: 16
+        },
+      EngMedium16: {
+          fontFamily: "InterMedium",
+          fontSize: 16
+      },
+      EngSemiBold16: {
+          fontFamily: "InterSemiBold",
+          fontSize: 16
+      },
+      EngBold16: {
+          fontFamily: "InterBold",
+          fontSize: 16
+      },
+
+      EngLight18: {
+        fontFamily: "InterLight",
+        fontSize: 18
+      },
+      EngRegular18: {
+          fontFamily: "InterRegular",
+          fontSize: 18
+        },
+      EngMedium18: {
+          fontFamily: "InterMedium",
+          fontSize: 18
+      },
+      EngSemiBold18: {
+          fontFamily: "InterSemiBold",
+          fontSize: 18
+      },
+      EngBold18: {
+          fontFamily: "InterBold",
+          fontSize: 18
+      },
+
+      EngLight22: {
+        fontFamily: "InterLight",
+        fontSize: 22
+      },
+      EngRegular22: {
+          fontFamily: "InterRegular",
+          fontSize: 22
+        },
+      EngMedium22: {
+          fontFamily: "InterMedium",
+          fontSize: 22
+      },
+      EngSemiBold22: {
+          fontFamily: "InterSemiBold",
+          fontSize: 22
+      },
+      EngBold22: {
+          fontFamily: "InterBold",
+          fontSize: 22
+      },
+
+      ThaiLight8: {
+        fontFamily: "IBMLight",
+        fontSize: 8
+      },
+      ThaiRegular8: {
+        fontFamily: "IBMRegular",
+        fontSize: 8
+      },
+      ThaiMedium8: {
+        fontFamily: "IBMMedium",
+        fontSize: 8
+      },
+      ThaiSemiBold8: {
+        fontFamily: "IBMSemiBold",
+        fontSize: 8
+      },
+      ThaiBold8: {
+        fontFamily: "IBMBold",
+        fontSize: 8
+      },
+
+      ThaiLight10: {
+        fontFamily: "IBMLight",
+        fontSize: 10
+      },
+      ThaiRegular10: {
+        fontFamily: "IBMRegular",
+        fontSize: 10
+      },
+      ThaiMedium10: {
+        fontFamily: "IBMMedium",
+        fontSize: 10
+      },
+      ThaiSemiBold10: {
+        fontFamily: "IBMSemiBold",
+        fontSize: 10
+      },
+      ThaiBold10: {
+        fontFamily: "IBMBold",
+        fontSize: 10
+      },
+
+      ThaiLight12: {
+        fontFamily: "IBMLight",
+        fontSize: 12
+      },
+      ThaiRegular12: {
+        fontFamily: "IBMRegular",
+        fontSize: 12
+      },
+      ThaiMedium12: {
+        fontFamily: "IBMMedium",
+        fontSize: 12
+      },
+      ThaiSemiBold12: {
+        fontFamily: "IBMSemiBold",
+        fontSize: 12
+      },
+      ThaiBold12: {
+        fontFamily: "IBMBold",
+        fontSize: 12
+      },
+
+      ThaiLight14: {
+        fontFamily: "IBMLight",
+        fontSize: 14
+      },
+      ThaiRegular14: {
+        fontFamily: "IBMRegular",
+        fontSize: 14
+      },
+      ThaiMedium14: {
+        fontFamily: "IBMMedium",
+        fontSize: 14
+      },
+      ThaiSemiBold14: {
+        fontFamily: "IBMSemiBold",
+        fontSize: 14
+      },
+      ThaiBold14: {
+        fontFamily: "IBMBold",
+        fontSize: 14
+      },
+
+      ThaiLight16: {
+        fontFamily: "IBMLight",
+        fontSize: 16
+      },
+      ThaiRegular16: {
+        fontFamily: "IBMRegular",
+        fontSize: 16
+      },
+      ThaiMedium16: {
+        fontFamily: "IBMMedium",
+        fontSize: 16
+      },
+      ThaiSemiBold16: {
+        fontFamily: "IBMSemiBold",
+        fontSize: 16
+      },
+      ThaiBold16: {
+        fontFamily: "IBMBold",
+        fontSize: 16
+      },
+
+      ThaiLight18: {
+        fontFamily: "IBMLight",
+        fontSize: 18
+      },
+      ThaiRegular18: {
+        fontFamily: "IBMRegular",
+        fontSize: 18
+      },
+      ThaiMedium18: {
+        fontFamily: "IBMMedium",
+        fontSize: 18
+      },
+      ThaiSemiBold18: {
+        fontFamily: "IBMSemiBold",
+        fontSize: 18
+      },
+      ThaiBold18: {
+        fontFamily: "IBMBold",
+        fontSize: 18
+      },
+
+      ThaiLight22: {
+        fontFamily: "IBMLight",
+        fontSize: 22
+      },
+      ThaiRegular22: {
+        fontFamily: "IBMRegular",
+        fontSize: 22
+      },
+      ThaiMedium22: {
+        fontFamily: "IBMMedium",
+        fontSize: 22
+      },
+      ThaiSemiBold22: {
+        fontFamily: "IBMSemiBold",
+        fontSize: 22
+      },
+      ThaiBold22: {
+        fontFamily: "IBMBold",
+        fontSize: 22
+      },
+  };
+  
+  export default fonts;
+  
