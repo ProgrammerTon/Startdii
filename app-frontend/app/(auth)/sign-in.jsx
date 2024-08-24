46bb17f839ed2568c2e6f8c084422c760cb40a91
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-08-25 02:05:59 +0700

    add sign in

--- app-frontend/app/(auth)/sign-in.jsx
+++ app-frontend/app/(auth)/sign-in.jsx
@@ -10,8 +10,11 @@ import FormField from "../../components/FormField";
 import React from "react";
 import { useState } from "react";
 import { ScrollView } from "react-native";
-import AuthService from "../../services/AuthService"
+import AuthService from "../../services/AuthService";
 import { router } from "expo-router";
+import fonts from "../../constants/font";
+import { Shadow } from 'react-native-shadow-2';
+import colors from "../../constants/color";
 
 export default function SignUp() {
   const [submitting, setSubmitting] = useState(false);
@@ -45,49 +48,91 @@ export default function SignUp() {
     }
   };
 
-  return (
-    <SafeAreaView className="h-full bg-[#FEDD3A]">
-      <ScrollView>
-        <View
-          className="flex-1 justify-center items-center px-11 my-6"
-          style={{
-            minHeight: Dimensions.get("window").height - 100,
-          }}
-        >
-          <Text className="text-2xl font-bold text-black">Sign in</Text>
-          <View className=" mx-4 my-5 p-5 rounded-[10px] bg-[#F6F6F6] w-full flex justify-center items-center">
-            <FormField
-              title="Email"
-              value={form.email}
-              handleChangeText={(e) => setForm({ ...form, email: e })}
-              otherStyles="mt-3"
-              keyboardType="email-address"
-            />
+  const styles = {
+    bg: {
+      height: '100%',
+      backgroundColor: colors.yellow,
+    },
+    container: {
+      flex: 1, // Equivalent to flex-1
+      justifyContent: 'center', // Equivalent to justify-center
+      alignItems: 'center', // Equivalent to items-center
+      paddingHorizontal: '3%',
+      paddingVertical: '80%',
+      marginVertical: '3%',
+      minHeight: Dimensions.get('window').height - 100,
+    },
+    frame: {
+      marginHorizontal: '3%',
+      marginVertical: '3%',
+      paddingHorizontal: '6%',
+      paddingVertical: '6%',
+      borderRadius: 10, // Equivalent to rounded-[10px]
+      backgroundColor: colors.gray_bg,
+      width: '100%',
+      height: '100%',
+      flex: 1, // Assuming you want to use flex to fill available space
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    signInText: {
+      fontSize: 30,
+      color: colors.black,
+      textAlign: "left",
+      alignSelf: "flex-start",
+      marginLeft: '9%',
+    },
+    box: {
+      position: 'absolute',
+      top: '208%',
+      left: '40%',
+    },
+  };
 
-            <FormField
-              title="Password"
-              value={form.password}
-              handleChangeText={(e) => setForm({ ...form, password: e })}
-              otherStyles="mt-3"
-            />
-            <View className="flex flex-row justify-between px-2 gap-11 mt-0.5">
-              <TouchableOpacity
-                className="bg-[#D9D9D9] h-[44px] w-[92px] flex justify-center items-center rounded-xl mt-5"
-                onPress={() => {
-                  router.replace("/");
-                }}
-              >
-                <Text>Cancel</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                className="bg-[#0270ED] h-[44px] w-[92px] flex justify-center items-center rounded-xl mt-5"
-                onPress={submit}
-              >
-                <Text className="text-white">Sign in</Text>
-              </TouchableOpacity>
+  return (
+    <SafeAreaView style={styles.bg}>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={[fonts.EngBold22, styles.signInText]}>Sign in</Text>
+          <Shadow
+            distance={20}
+            startColor="rgba(145, 145, 145, 0.05)"
+            offset={[0, 0]}
+          >
+            <View style={styles.frame}>
+
+              <FormField
+                title="Email"
+                value={form.email}
+                handleChangeText={(e) => setForm({ ...form, email: e })}
+                otherStyles="mt-3"
+                keyboardType="email-address"
+              // style={{
+              //   shadowColor: "#919191", // Shadow color
+              //   shadowOpacity: 0.25, // Shadow opacity (25%)
+              //   shadowOffset: { width: 0, height: 0 }, // Offset (x, y)
+              //   shadowRadius: 8, // Blur radius
+              //   elevation: 4, // Elevation for Android (controls shadow depth)
+              // }}
+              />
+
+              <FormField
+                title="Password"
+                value={form.password}
+                handleChangeText={(e) => setForm({ ...form, password: e })}
+                otherStyles="mt-3"
+              />
             </View>
-          </View>
-          <View>
+          </Shadow>
+          <View style={styles.box}>
+              <View className="flex flex-row justify-between px-2 gap-11 mt-0.5">
+                <TouchableOpacity
+                  className="bg-[#0270ED] h-[44px] w-[92px] flex justify-center items-center rounded-[50px] mt-5"
+                  onPress={submit}
+                  >
+                  <Text className="text-white">Sign in</Text>
+                </TouchableOpacity>
+              </View>
             <Text className="text-xl text-black">{user?.email}</Text>
           </View>
         </View>
