afb7658e5a1730fafe46012619cf981f0f01e975
236sec <popole159za@gmail.com>
2024-08-24 08:40:25 +0700

    checking for login in home page

--- app-frontend/app/(tabs)/home.jsx
+++ app-frontend/app/(tabs)/home.jsx
@@ -1,11 +1,22 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { router } from "expo-router";
 
 export default function Home() {
+  const { user, isLogged } = useGlobalContext();
+
+  useEffect(() => {
+    if (!isLogged) {
+      router.replace("/sign-in");
+    }
+  }, []);
+
   return (
     <SafeAreaView className="flex-1 justify-center items-center">
       <Text className="text-xl">Home</Text>
+      <Text></Text>
     </SafeAreaView>
   );
 }
