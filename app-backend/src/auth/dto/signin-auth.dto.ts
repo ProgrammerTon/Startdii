d7aa0f0de957f76c9e915ad8eeb0102f7ee9a2ed
236sec <popole159za@gmail.com>
2024-08-24 15:57:39 +0700

    Add Swagger Schema for Auth

--- app-backend/src/auth/dto/signin-auth.dto.ts
+++ app-backend/src/auth/dto/signin-auth.dto.ts
@@ -1,10 +1,13 @@
 import { IsNotEmpty, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignInAuthDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 }
