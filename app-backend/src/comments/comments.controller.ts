970230aa22c25cc210b8c8329016816c5f57437d
236sec <popole159za@gmail.com>
2024-08-24 15:57:49 +0700

    Add Swagger Schema for Comment

--- app-backend/src/comments/comments.controller.ts
+++ app-backend/src/comments/comments.controller.ts
@@ -1,17 +1,11 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { ObjectId } from 'mongodb';
 import { ParseObjectIdPipe } from 'src/common/pipes';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Comment')
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
