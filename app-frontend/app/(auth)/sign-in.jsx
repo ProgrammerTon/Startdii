86a706503cdcf13e8a63bf4cdb6a71d5bf4ffe8b
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-08-25 22:50:57 +0700

    add sign in

--- app-frontend/app/(auth)/sign-in.jsx
+++ app-frontend/app/(auth)/sign-in.jsx
@@ -7,6 +7,7 @@ import {
   Alert,
 } from "react-native";
 import FormField from "../../components/FormField";
+import Button from "../../components/Button";
 import React from "react";
 import { useState } from "react";
 import { ScrollView } from "react-native";
@@ -54,6 +55,7 @@ export default function SignUp() {
       backgroundColor: colors.yellow,
     },
     container: {
+      position: 'relative',
       flex: 1, // Equivalent to flex-1
       justifyContent: 'center', // Equivalent to justify-center
       alignItems: 'center', // Equivalent to items-center
@@ -63,29 +65,54 @@ export default function SignUp() {
       minHeight: Dimensions.get('window').height - 100,
     },
     frame: {
-      marginHorizontal: '3%',
-      marginVertical: '3%',
-      paddingHorizontal: '6%',
-      paddingVertical: '6%',
+      margin: '3%',
+      paddingHorizontal: '1%',
+      paddingTop: '18%',
+      paddingBottom: '20%',
       borderRadius: 10, // Equivalent to rounded-[10px]
       backgroundColor: colors.gray_bg,
-      width: '100%',
+      width: '95%',
       height: '100%',
       flex: 1, // Assuming you want to use flex to fill available space
       justifyContent: 'center',
       alignItems: 'center',
+      shadowColor: colors.gray_bgblur,
+      shadowOffset: { width: 0, height: 0 },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
     },
     signInText: {
-      fontSize: 30,
+      fontSize: 34,
       color: colors.black,
       textAlign: "left",
       alignSelf: "flex-start",
-      marginLeft: '9%',
+      marginLeft: '7%',
     },
     box: {
       position: 'absolute',
-      top: '208%',
-      left: '40%',
+      top: '183%',
+      left: '55%',
+      transform: [{ translateX: -50 }, { translateY: -50 }], // This will center the box
+    },
+    box2: {
+      alignSelf: 'flex-end',
+      right: '5%',
+      bottom: '5%',
+    },
+    text: {
+      color: colors.blue,
+      textDecorationLine: 'underline',
+    },
+    text2: {
+      color: colors.black,
+      marginRight: '3%',
+    },
+    box3: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      top: '8%',
     },
   };
 
@@ -94,46 +121,43 @@ export default function SignUp() {
       <ScrollView>
         <View style={styles.container}>
           <Text style={[fonts.EngBold22, styles.signInText]}>Sign in</Text>
-          <Shadow
-            distance={20}
-            startColor="rgba(145, 145, 145, 0.05)"
-            offset={[0, 0]}
-          >
-            <View style={styles.frame}>
+          <View style={styles.frame}>
 
-              <FormField
-                title="Email"
-                value={form.email}
-                handleChangeText={(e) => setForm({ ...form, email: e })}
-                otherStyles="mt-3"
-                keyboardType="email-address"
-              // style={{
-              //   shadowColor: "#919191", // Shadow color
-              //   shadowOpacity: 0.25, // Shadow opacity (25%)
-              //   shadowOffset: { width: 0, height: 0 }, // Offset (x, y)
-              //   shadowRadius: 8, // Blur radius
-              //   elevation: 4, // Elevation for Android (controls shadow depth)
-              // }}
-              />
+            <FormField
+              title="Email"
+              value={form.email}
+              handleChangeText={(e) => setForm({ ...form, email: e })}
+              otherStyles="mt-3"
+              keyboardType="email-address"
+            />
 
-              <FormField
-                title="Password"
-                value={form.password}
-                handleChangeText={(e) => setForm({ ...form, password: e })}
-                otherStyles="mt-3"
-              />
-            </View>
-          </Shadow>
+            <FormField
+              title="Password"
+              value={form.password}
+              handleChangeText={(e) => setForm({ ...form, password: e })}
+              otherStyles="mt-3"
+            />
+
+            <TouchableOpacity
+              style={styles.box2}
+              onPress={submit}
+            >
+              <Text style={[fonts.EngMedium12, styles.text]}>Forgot Password ?</Text>
+            </TouchableOpacity>
+
+          </View>
           <View style={styles.box}>
-              <View className="flex flex-row justify-between px-2 gap-11 mt-0.5">
-                <TouchableOpacity
-                  className="bg-[#0270ED] h-[44px] w-[92px] flex justify-center items-center rounded-[50px] mt-5"
-                  onPress={submit}
-                  >
-                  <Text className="text-white">Sign in</Text>
-                </TouchableOpacity>
-              </View>
+            <Button
+              title="Sign in"
+              onPress={submit}
+            />
             <Text className="text-xl text-black">{user?.email}</Text>
+          </View>
+          <View style={styles.box3}>
+            <Text style={[fonts.EngMedium14, styles.text2]}>Don’t have an account?</Text>
+            <TouchableOpacity onPress={submit}>
+              <Text style={[fonts.EngMedium14, styles.text]}>Create an account</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
