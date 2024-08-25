3a08be3ef5bc81f86b8987fc365934df0eeb0eb6
236sec <popole159za@gmail.com>
2024-08-26 06:59:47 +0700

    fix syntax

--- app-frontend/app/index.jsx
+++ app-frontend/app/index.jsx
@@ -4,9 +4,8 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
-import * as Font from 'expo-font';
+import * as Font from "expo-font";
 import { useGlobalContext } from "../context/GlobalProvider";
-
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -15,16 +14,16 @@ export default function App() {
   useEffect(() => {
     async function loadFonts() {
       await Font.loadAsync({
-        InterLight: require('../assets/fonts/Inter_18pt-Light.ttf'),
-        InterRegular: require('../assets/fonts/Inter_18pt-Regular.ttf'),
-        InterMedium: require('../assets/fonts/Inter_18pt-Medium.ttf'),
-        InterSemiBold: require('../assets/fonts/Inter_18pt-SemiBold.ttf'),
-        InterBold: require('../assets/fonts/Inter_18pt-Bold.ttf'),
-        IBMLight: require('../assets/fonts/IBMPlexSansThai-Light.ttf'),
-        IBMRegular: require('../assets/fonts/IBMPlexSansThai-Regular.ttf'),
-        IBMMedium: require('../assets/fonts/IBMPlexSansThai-Medium.ttf'),
-        IBMSemiBold: require('../assets/fonts/IBMPlexSansThai-SemiBold.ttf'),
-        IBMBold: require('../assets/fonts/IBMPlexSansThai-Bold.ttf'),
+        InterLight: require("../assets/fonts/Inter_18pt-Light.ttf"),
+        InterRegular: require("../assets/fonts/Inter_18pt-Regular.ttf"),
+        InterMedium: require("../assets/fonts/Inter_18pt-Medium.ttf"),
+        InterSemiBold: require("../assets/fonts/Inter_18pt-SemiBold.ttf"),
+        InterBold: require("../assets/fonts/Inter_18pt-Bold.ttf"),
+        IBMLight: require("../assets/fonts/IBMPlexSansThai-Light.ttf"),
+        IBMRegular: require("../assets/fonts/IBMPlexSansThai-Regular.ttf"),
+        IBMMedium: require("../assets/fonts/IBMPlexSansThai-Medium.ttf"),
+        IBMSemiBold: require("../assets/fonts/IBMPlexSansThai-SemiBold.ttf"),
+        IBMBold: require("../assets/fonts/IBMPlexSansThai-Bold.ttf"),
       });
 
       setFontsLoaded(true);
@@ -68,3 +67,4 @@ export default function App() {
       </SafeAreaView>
     );
   }
+}
