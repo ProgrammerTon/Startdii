bcb292a187a1f2a3c96e9718e063104deb14dee7
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-08-27 00:57:26 +0700

    add button

--- app-frontend/components/Button.jsx
+++ app-frontend/components/Button.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
 import fonts from "../constants/font";
 import colors from "../constants/color";
 
 const Button = ({ onPress, title, style, textStyle, color, textcolor }) => {
+  const [textWidth, setTextWidth] = useState(0);
+  const textRef = useRef(null);
+  const handleTextLayout = (event) => {
+    setTextWidth(event.nativeEvent.layout.width);
+  };
+
   return (
-    <TouchableOpacity style={[styles.button, style, { backgroundColor: color }]} onPress={onPress}>
-      <Text style={[fonts.EngMedium18, textStyle, { color: textcolor }]}>{title}</Text>
+    <TouchableOpacity
+      style={[
+        styles.button,
+        style,
+        { backgroundColor: color, width: textWidth + 60 }
+      ]}
+      onPress={onPress}
+    >
+      <View onLayout={handleTextLayout}>
+        <Text ref={textRef} style={[fonts.EngMedium18, textStyle, { color: textcolor }]}>
+          {title}
+        </Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    height: '100%',
-    width: '195%',
+    height: 60,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 50,
@@ -22,3 +38,4 @@ const styles = StyleSheet.create({
 });
 
 export default Button;
+
