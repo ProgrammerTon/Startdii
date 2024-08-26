d856a82243913dd2e9e784e5875ebd65040ccfca
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-08-27 00:40:54 +0700

    add button

--- app-frontend/components/Button.jsx
+++ app-frontend/components/Button.jsx
@@ -3,25 +3,21 @@ import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 import fonts from "../constants/font";
 import colors from "../constants/color";
 
-const Button = ({ onPress, title, style, textStyle }) => {
+const Button = ({ onPress, title, style, textStyle, color, textcolor }) => {
   return (
-    <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
-      <Text style={[fonts.EngMedium18, styles.buttonText, textStyle]}>{title}</Text>
+    <TouchableOpacity style={[styles.button, style, { backgroundColor: color }]} onPress={onPress}>
+      <Text style={[fonts.EngMedium18, textStyle, { color: textcolor }]}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: colors.blue,
     height: '100%',
     width: '195%',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 50,
-  },
-  buttonText: {
-    color: 'white',
   },
 });
 
