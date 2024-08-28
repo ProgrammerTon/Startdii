53e25afc2c9b8f1ceb20b37246b0ead71454fcd2
236sec <popole159za@gmail.com>
2024-08-28 16:36:10 +0700

    create chatlist entity

--- app-backend/src/users/users.module.ts
+++ app-backend/src/users/users.module.ts
@@ -4,11 +4,13 @@ import { User, UserSchema } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Source, SourceSchema } from 'src/sources/entities/source.entity';
+import { ChatList, ChatListSchema } from './entities/chatlist.entity';
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
       { name: Source.name, schema: SourceSchema },
+      { name: ChatList.name, schema: ChatListSchema },
     ]),
   ],
   providers: [UsersService],
