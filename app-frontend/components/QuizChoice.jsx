b233b5dae37ec14996792cd84f887c7fcdeb3f91
Tanwa2545 <goopkus@gmail.com>
2024-09-01 19:10:37 +0700

    feat QuizChoice can display a 2 column multiple choice

--- app-frontend/components/QuizChoice.jsx
+++ app-frontend/components/QuizChoice.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, Dimensions } from 'react-native';
 import fonts from "../constants/font";
 import colors from "../constants/color";
 
-const QuizChoice = ({ content, isSelected, onPress }) => {
+const { width, height } = Dimensions.get('window');
+
+const QuizChoice = ({ content, isSelected, onPress, makeColumn}) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <View style={[styles.choiceContainer, isSelected && styles.selectedContainer]}>
+      <View style={[styles.choiceContainer, isSelected && styles.selectedContainer, makeColumn && styles.makeColumn]}>
         <Text style={styles.textStyle}> {content} </Text>
       </View>
     </TouchableOpacity>
@@ -17,16 +19,20 @@ const styles = StyleSheet.create({
   choiceContainer:{
     backgroundColor:"#fff",
     paddingVertical: 20,
+    paddingHorizontal: 10,
     marginTop:10,
     borderWidth:3,
-    borderColor:"#04B36E",
+    borderColor:"#04B36E", // Green
     borderStyle:"solid",
     borderRadius:10,
     flexDirection:"row",
     justifyContent:"center",
   },
   selectedContainer: {
-    backgroundColor: "#04B36E", // Change to desired color for selected state
+    backgroundColor: "#04B36E", // Green
+  },
+  makeColumn: {
+    width: width * 0.4,
   },
   textStyle:{
     fontSize: 20,
