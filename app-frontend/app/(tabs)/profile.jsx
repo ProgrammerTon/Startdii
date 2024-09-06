daa0fe1231bb4a862a9188e2546717c6ffb58702
236sec <popole159za@gmail.com>
2024-09-06 16:31:13 +0700

    Move File to Tabs

--- app-frontend/app/(tabs)/profile.jsx
+++ app-frontend/app/(tabs)/profile.jsx
@@ -1,10 +1,78 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { router } from "expo-router";
 import React from "react";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import SafeAreaViewAndroid from "../../components/SafeAreaViewAndroid";
 
 export default function Profile() {
+  const { isLogged, user } = useGlobalContext();
   return (
-    <View>
+    <SafeAreaViewAndroid>
       <Text>Profile</Text>
-    </View>
+      <TouchableHighlight
+        onPress={() => router.push("/trynaja")}
+        className="bg-green-600 p-2 rounded-xl"
+      >
+        <Text className="text-lg">TNJ</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight
+        onPress={() => router.push("/sources/create")}
+        className="bg-green-600 p-2 rounded-xl"
+      >
+        <Text className="text-lg">Create Note</Text>
+      </TouchableHighlight>
+      {isLogged ? <Text>Already Login</Text> : <Text>Not Login</Text>}
+      {isLogged ? <Text>{user?.email}</Text> : null}
+      <TouchableHighlight
+        onPress={() => router.push("/ArchiveSystem/ArchiveMainPage")}
+        className="bg-green-600 p-2 rounded-xl"
+      >
+        <Text className="text-lg">Archive</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        onPress={() => router.push("/ArchiveSystem/D2_QuizMaker")}
+        className="bg-green-600 p-2 rounded-xl"
+      >
+        <Text className="text-lg">D2_QuizMaker</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        onPress={() => router.push("/chatsystem/H1_user")}
+        className="bg-green-600 p-2 rounded-xl"
+      >
+        <Text className="text-lg">Chat 2</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        onPress={() => router.push("/guild/I1_GuildPage")}
+        className="bg-green-600 p-2 rounded-xl"
+      >
+        <Text className="text-lg">Guild</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        onPress={() => router.push("/UserFinderSystem/G1_UserFinder")}
+        className="bg-green-600 p-2 rounded-xl"
+      >
+        <Text className="text-lg">UserFinder</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        onPress={() => router.push("/ArchiveSystem/E2_NoteDetailed")}
+        className="bg-green-600 p-2 rounded-xl"
+      >
+        <Text className="text-lg">NoteDetailed</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        onPress={() => router.push("/bottomtab/tab")}
+        className="bg-green-600 p-2 rounded-xl"
+      >
+        <Text className="text-lg">test</Text>
+      </TouchableHighlight>
+    </SafeAreaViewAndroid>
   );
 }
--- app-frontend/components/TagList.jsx
+++ app-frontend/components/TagList.jsx
@@ -6,7 +6,10 @@ export default function TagList({ tags, title, id }) {
     <View className="flex flex-row gap-3">
       {tags.map((tag, i) => {
         return (
-          <View className="bg-[#D9D9D9] p-1 rounded-xl" key={`${tag}-${id}`}>
+          <View
+            className="bg-[#D9D9D9] p-1 rounded-xl"
+            key={`${tag}-${id}-${i}`}
+          >
             <Text className="font-semibold">#{tag}</Text>
           </View>
         );
