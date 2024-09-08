8c2454936aafb284285936d4783e42a30e074e58
ProgrammerTon <pattarapol.nanetra@gmail.com>
2024-09-08 10:43:29 +0700

    QuizFlow Dummy

---
+++ app-frontend/app/quizzes/F3_quizflow.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+const QuizFlow = () => {
+
+  
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+export default QuizFlow;
+
+const styles = StyleSheet.create({
+
+});
