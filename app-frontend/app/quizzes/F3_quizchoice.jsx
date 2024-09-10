bb9582660ded98a0ed84303c0dcdb72231e94537
236sec <popole159za@gmail.com>
2024-09-10 21:57:46 +0700

    Fixed Choice

--- app-frontend/app/quizzes/F3_quizchoice.jsx
+++ app-frontend/app/quizzes/F3_quizchoice.jsx
@@ -5,16 +5,20 @@ import {
   TouchableOpacity,
   Dimensions,
   StyleSheet,
-  Modal
+  Modal,
 } from "react-native";
-import { React , useState } from "react";
+import { React, useState, useEffect } from "react";
 import QuizChoice from "../../components/QuizChoice";
-import AntDesign from '@expo/vector-icons/AntDesign';
+import AntDesign from "@expo/vector-icons/AntDesign";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
-export default function QuizChoices({ questionData, onSubmit, questionNumber, totalQuestions }) {
-
+export default function QuizChoices({
+  questionData,
+  onSubmit,
+  questionNumber,
+  totalQuestions,
+}) {
   const [selectedChoice, setSelectedChoice] = useState([]);
   const [closeQuiz, setCloseQuiz] = useState(false);
 
@@ -22,7 +26,9 @@ export default function QuizChoices({ questionData, onSubmit, questionNumber, to
     // Allow multiple selections
     if (questionData.answer.length >= -1) {
       if (selectedChoice.includes(index)) {
-        const newSelectedChoice = selectedChoice.filter((item) => item !== index);
+        const newSelectedChoice = selectedChoice.filter(
+          (item) => item !== index
+        );
         setSelectedChoice(newSelectedChoice); // Unselect if the same index is pressed
       } else {
         const newSelectedChoice = [...selectedChoice, index].sort(); // Sort the array after adding the new index
@@ -37,60 +43,94 @@ export default function QuizChoices({ questionData, onSubmit, questionNumber, to
     }
   };
 
+  useEffect(() => {
+    setSelectedChoice([]);
+  }, [questionData]);
+
   return (
     <View style={styles.container}>
       <View style={styles.topPart}>
         <View style={styles.closeQuiz}>
-          <TouchableOpacity style={{backgroundColor: "#fff", borderRadius:30}} onPress={() => setCloseQuiz(true)}>
+          <TouchableOpacity
+            style={{ backgroundColor: "#fff", borderRadius: 30 }}
+            onPress={() => setCloseQuiz(true)}
+          >
             <AntDesign name="closecircle" size={30} color="red" />
           </TouchableOpacity>
         </View>
         <View style={styles.quizNumber}>
-            <Text style={styles.textNumber}>{questionNumber} / {totalQuestions}</Text>
+          <Text style={styles.textNumber}>
+            {questionNumber} / {totalQuestions}
+          </Text>
         </View>
         <View style={styles.question}>
-            <Text style={styles.textStyle}> {questionData.question} </Text>
+          <Text style={styles.textStyle}> {questionData.question} </Text>
         </View>
       </View>
-      
+
       <View style={styles.bottomPart}>
         <View style={styles.choice}>
-        <ScrollView>
-        {questionData.choice.map((item, index) => {
-          return <QuizChoice 
+          <ScrollView>
+            {questionData.choice.map((item, index) => {
+              return (
+                <QuizChoice
                   key={index}
                   content={item}
-                  isSelected={selectedChoice.includes(index)} 
+                  isSelected={selectedChoice.includes(index)}
                   onPress={() => handleChoiceSelect(index)}
-                  />
-          })}
-        </ScrollView>
+                />
+              );
+            })}
+          </ScrollView>
         </View>
         <View>
-          <TouchableOpacity style={styles.nextButton} onPress={() => onSubmit(selectedChoice)}>
-            <Text style={{fontSize: 16, color: "#fff"}}> Next </Text>
+          <TouchableOpacity
+            style={styles.nextButton}
+            onPress={() => onSubmit(selectedChoice)}
+          >
+            <Text style={{ fontSize: 16, color: "#fff" }}> Next </Text>
           </TouchableOpacity>
         </View>
       </View>
       <Modal transparent={true} visible={closeQuiz}>
-        <View style={{flex: 1, backgroundColor: "#555555aa"}}>
+        <View style={{ flex: 1, backgroundColor: "#555555aa" }}>
           <View style={styles.leaveQuizPopUp}>
             <View>
-              <Text style={{fontSize: 20, fontWeight: "bold"}}> Do you want to Leave Quiz? </Text>
+              <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                {" "}
+                Do you want to Leave Quiz?{" "}
+              </Text>
             </View>
-            <View style={{flexDirection: "row", justifyContent: "space-between", marginTop: 20 }}>
-              <TouchableOpacity style={styles.closeQuizButton} onPress={() => setCloseQuiz(false)}>
-                <Text style={{fontSize: 16, fontWeight: "bold"}}> Cancel </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginTop: 20,
+              }}
+            >
+              <TouchableOpacity
+                style={styles.closeQuizButton}
+                onPress={() => setCloseQuiz(false)}
+              >
+                <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                  {" "}
+                  Cancel{" "}
+                </Text>
               </TouchableOpacity>
               <TouchableOpacity style={styles.leaveQuizButton}>
-                <Text style={{fontSize: 16, fontWeight: "bold", color: "#fff"}}> Leave </Text>
+                <Text
+                  style={{ fontSize: 16, fontWeight: "bold", color: "#fff" }}
+                >
+                  {" "}
+                  Leave{" "}
+                </Text>
               </TouchableOpacity>
             </View>
           </View>
         </View>
       </Modal>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -99,14 +139,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#eee",
     alignItems: "center",
     justifyContent: "center",
-    marginHorizontal: '1%',
+    marginHorizontal: "1%",
   },
-  topPart:{
+  topPart: {
     height: height * 0.3,
     width: width,
     backgroundColor: "#04B36E",
   },
-  bottomPart:{
+  bottomPart: {
     height: height * 0.7,
     width: width,
     justifyContent: "center",
@@ -116,13 +156,13 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignSelf: "flex-end",
     marginRight: width * 0.05,
-    marginTop: height * 0.07
+    marginTop: height * 0.07,
   },
-  quizNumber:{
+  quizNumber: {
     alignSelf: "center",
     backgroundColor: "#ddd",
-    padding:7,
-    paddingHorizontal:15,
+    padding: 7,
+    paddingHorizontal: 15,
     marginTop: -height * 0.2,
     marginBottom: height * 0.08,
     zIndex: 1,
@@ -137,16 +177,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingVertical: height * 0.02,
     marginVertical: -height * 0.1,
-    borderRadius:10,
-    borderWidth:2,
-    borderColor:"black",
-    borderStyle:"solid",
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "black",
+    borderStyle: "solid",
   },
-  textNumber:{
+  textNumber: {
     fontSize: 20,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
-  textStyle:{
+  textStyle: {
     fontSize: 20,
     //fontWeight: "bold"
   },
@@ -159,13 +199,13 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignSelf: "center",
   },
-  nextButton:{
+  nextButton: {
     paddingHorizontal: width * 0.05,
-    paddingVertical:10,
-    marginRight:20,
+    paddingVertical: 10,
+    marginRight: 20,
     marginVertical: -height * 0.13,
-    backgroundColor: "#0270ED", 
-    borderRadius:20,
+    backgroundColor: "#0270ED",
+    borderRadius: 20,
     alignSelf: "flex-end",
   },
   headerText: {
@@ -174,34 +214,34 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
   },
-  closeQuizButton:{
+  closeQuizButton: {
     paddingHorizontal: width * 0.05,
-    paddingVertical:10,
+    paddingVertical: 10,
     marginVertical: 5,
     marginHorizontal: 20,
     backgroundColor: "#bbb",
     paddingHorizontal: width * 0.05,
-    borderRadius:20,
+    borderRadius: 20,
   },
-  leaveQuizButton:{
+  leaveQuizButton: {
     paddingHorizontal: width * 0.05,
-    paddingVertical:10,
+    paddingVertical: 10,
     marginVertical: 5,
     marginHorizontal: 20,
     backgroundColor: "#F44D19",
     paddingHorizontal: width * 0.05,
-    borderRadius:20,
+    borderRadius: 20,
   },
-  leaveQuizPopUp:{
-    backgroundColor: "#fff", 
-    marginTop: height*0.4, 
-    margin:50, 
-    padding: 20, 
-    alignItems: "center", 
-    borderRadius: 10, 
-    height: height * 0.15
-  }
-})
+  leaveQuizPopUp: {
+    backgroundColor: "#fff",
+    marginTop: height * 0.4,
+    margin: 50,
+    padding: 20,
+    alignItems: "center",
+    borderRadius: 10,
+    height: height * 0.15,
+  },
+});
 {
   /* <FlatList
           data={quizData[currentQuestion].choice}
@@ -210,4 +250,5 @@ const styles = StyleSheet.create({
           )}
           keyExtractor={(item)=>item.id}
           ListHeaderComponent={<Text style={styles.headerText}>Choice</Text>}
-        /> */}
+        /> */
+}
