37ce39c2d7d9eb54754895e98999ddf4a9bc8bff
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-09-13 11:05:13 +0700

    add index

--- app-frontend/app/index.jsx
+++ app-frontend/app/index.jsx
@@ -6,6 +6,7 @@ import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
 import * as Font from "expo-font";
 import { useGlobalContext } from "../context/GlobalProvider";
+import { CharacterProvider } from "./profile/charcontext";
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -34,60 +35,61 @@ export default function App() {
 
   if (fontsLoaded) {
     return (
-      <SafeAreaView className="flex-1 items-center justify-center">
-        <StatusBar style="auto" />
-        <Text className="text-3xl">Hello Welcome to</Text>
-        <Text className="text-3xl">Startdii</Text>
-        <TouchableHighlight
-          onPress={() => router.push("/home")}
-          className="bg-green-600 p-2 rounded-xl"
-        >
-          <Text className="text-lg">Home</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          onPress={() => router.push("/sign-up")}
-          className="bg-green-600 p-2 rounded-xl"
-        >
-          <Text className="text-lg">Sign Up</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          onPress={() => router.push("/sign-in")}
-          className="bg-green-600 p-2 rounded-xl"
-        >
-          <Text className="text-lg">Sign In</Text>
-        </TouchableHighlight>
+      <CharacterProvider>
+        <SafeAreaView className="flex-1 items-center justify-center">
+          <StatusBar style="auto" />
+          <Text className="text-3xl">Hello Welcome to</Text>
+          <Text className="text-3xl">Startdii</Text>
+          <TouchableHighlight
+            onPress={() => router.push("/home")}
+            className="bg-green-600 p-2 rounded-xl"
+          >
+            <Text className="text-lg">Home</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => router.push("/sign-up")}
+            className="bg-green-600 p-2 rounded-xl"
+          >
+            <Text className="text-lg">Sign Up</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => router.push("/sign-in")}
+            className="bg-green-600 p-2 rounded-xl"
+          >
+            <Text className="text-lg">Sign In</Text>
+          </TouchableHighlight>
 
-        <TouchableHighlight
-          onPress={() => router.push("/dress")}
-          className="bg-green-600 p-2 rounded-xl"
-        >
-          <Text className="text-lg">dress</Text>
-        </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => router.push("/sources/create")}
+            className="bg-green-600 p-2 rounded-xl"
+          >
+            <Text className="text-lg">Create Note</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => router.push("/bottomtab/tab")}
+            className="bg-green-600 p-2 rounded-xl"
+          >
+            <Text className="text-lg">test</Text>
+          </TouchableHighlight>
 
-        
+          <TouchableHighlight
+            onPress={() => router.push("/profile/profile_test")}
+            className="bg-green-600 p-2 rounded-xl"
+          >
+            <Text className="text-lg">Profile</Text>
+          </TouchableHighlight>
 
-      
-        <TouchableHighlight
-          onPress={() => router.push("/sources/create")}
-          className="bg-green-600 p-2 rounded-xl"
-        >
-          <Text className="text-lg">Create Note</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          onPress={() => router.push("/bottomtab/tab")}
-          className="bg-green-600 p-2 rounded-xl"
-        >
-          <Text className="text-lg">test</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          onPress={() => router.push("/profile/profile_test")}
-          className="bg-green-600 p-2 rounded-xl"
-        >
-          <Text className="text-lg">Profile</Text>
-        </TouchableHighlight>
-        {isLogged ? <Text>Already Login</Text> : <Text>Not Login</Text>}
-        {isLogged ? <Text>{user?.email}</Text> : null}
-      </SafeAreaView>
+          <TouchableHighlight
+            onPress={() => router.push("/dress")}
+            className="bg-green-600 p-2 rounded-xl"
+          >
+            <Text className="text-lg">dress</Text>
+          </TouchableHighlight>
+
+          {isLogged ? <Text>Already Login</Text> : <Text>Not Login</Text>}
+          {isLogged ? <Text>{user?.email}</Text> : null}
+        </SafeAreaView>
+      </CharacterProvider>
     );
   }
 }
