3e5523fffab00587c6087dcbad9c2480a0c08160
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-09-17 23:20:27 +0700

    add layout

--- app-frontend/app/_layout.jsx
+++ app-frontend/app/_layout.jsx
@@ -2,16 +2,19 @@ import { View, Text, SafeAreaView, StatusBar } from "react-native";
 import React from "react";
 import { SplashScreen, Stack } from "expo-router";
 import GlobalProvider from "../context/GlobalProvider";
+import { CharacterProvider } from "./profile/charcontext";
 
 export default function RootLayout() {
   return (
     <GlobalProvider>
-      <Stack>
-        <Stack.Screen name="guild" options={{ headerShown: true }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
+      <CharacterProvider>
+        <Stack>
+          <Stack.Screen name="guild" options={{ headerShown: true }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </CharacterProvider>
     </GlobalProvider>
   );
 }
