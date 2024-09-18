62406df5e96a07cdd73260fd802eddc17bc73992
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-09-18 21:20:07 +0700

    add weeklygoal

---
+++ app-frontend/components/WeeklyGoal.jsx
@@ -0,0 +1,32 @@
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+} from "react-native";
+import React, { useState } from "react";
+import colors from "../constants/color";
+import fonts from "../constants/font";
+import GoalProcess from "./GoalProcess";
+
+export default function WeeklyGoals() {
+    return (
+        <View style={styles.container}>
+            <GoalProcess
+                title={'ทำครบ 3 Quiz'}
+                percent={'33%'} />
+            <GoalProcess
+                title={'ทำครบ 3 Quiz'}
+                percent={'33%'} />
+            <GoalProcess
+                title={'ทำครบ 3 Quiz'}
+                percent={'33%'} />
+        </View>
+    )
+}
+
+const styles = {
+    container: {
+        alignItems: 'center',
+    },
+}
