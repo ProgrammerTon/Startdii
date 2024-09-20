ab69bb48c3e0f5cd9373e494299b4073dee2201c
ProgrammerTon <pattarapol.nanetra@gmail.com>
2024-09-20 12:23:42 +0700

    dress back

--- app-frontend/components/BackButton.jsx
+++ app-frontend/components/BackButton.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 import colors from '../constants/color';
 import Svg, { Path } from 'react-native-svg';
-
-const BackButton = ({ onBackPress }) => {
+import { router } from 'expo-router';
+const BackButton = () => {
 
   return (
-    <TouchableOpacity style={styles.button} onPress={onBackPress} >
+    <TouchableOpacity style={styles.button} onPress={() => router.back()} >
       <Svg
         style={styles.iconContainer}
         viewBox="0 0 22 19"
