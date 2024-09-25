edb7726c4731de4ee53f1aef60c6e41d5c074927
ProgrammerTon <pattarapol.nanetra@gmail.com>
2024-09-25 18:05:31 +0700

    QuizEditMasterTon

--- app-frontend/app/inventoryquiz/EditQuizComponent.jsx
+++ app-frontend/app/inventoryquiz/EditQuizComponent.jsx
@@ -1,35 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TouchableOpacity, StyleSheet, Text } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { router } from 'expo-router';
 
-const EditQuizComponent = ({quizId}) => {
+const EditQuizComponent = ({ quizId }) => {
+  const handleEditPress = () => {
+    console.log(`Edit Pressed ${quizId}`);
+    router.push(`/inventoryquiz/D1_QuizDesEdit?quizId=${quizId}`);
+  };
 
   return (
-    <>
-      <TouchableOpacity style={styles.reportButton} 
-        onPress={() => console.log(`Edit Pressed ${quizId}`)}
-        
-        >
-        <AntDesign name="edit" size={24} color="black" />
-      </TouchableOpacity>
-    </>
+    <TouchableOpacity style={styles.editButton} onPress={handleEditPress}>
+      <AntDesign name="edit" size={24} color="black" />
+      <Text style={styles.editText}>Edit</Text>
+    </TouchableOpacity>
   );
 };
 
 export default EditQuizComponent;
 
 const styles = StyleSheet.create({
-  reportButton: {
+  editButton: {
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "white",
+    backgroundColor: "#fff",
     paddingHorizontal: 10,
     paddingVertical: 5,
     borderRadius: 5,
     marginLeft: 10,
   },
-  buttonText: {
-    color: "white",
+  editText: {
+    color: "#000",
     fontSize: 14,
     marginLeft: 5,
   },
