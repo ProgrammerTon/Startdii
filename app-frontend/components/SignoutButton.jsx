ce450c6a6f6fb0dd7727840fbb75d366695c5975
Spchfonn <57552344+Spchfonn@users.noreply.github.com>
2024-10-01 23:23:38 +0700

    edit recheckbox

--- app-frontend/components/SignoutButton.jsx
+++ app-frontend/components/SignoutButton.jsx
@@ -7,7 +7,7 @@ import SignUp from "../app/(auth)/sign-in";
 import { useGlobalContext } from "../context/GlobalProvider";
 import { logoutUser } from "../utils/asyncstroage";
 import { router } from "expo-router";
-import RecheckBox from "./recheckbox";
+import RecheckBox from "./RecheckBox";
 
 const SignoutButton = ({ onPress, style, navigation }) => {
   const { setUser, setIsLogged } = useGlobalContext();
