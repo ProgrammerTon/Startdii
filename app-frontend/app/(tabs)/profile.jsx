13f4b91b760aadb8804a5af6d8c329ebf5375d66
Tanwa2545 <goopkus@gmail.com>
2024-10-12 23:44:07 +0700

    update styles

--- app-frontend/app/(tabs)/profile.jsx
+++ app-frontend/app/(tabs)/profile.jsx
@@ -248,19 +248,18 @@ export default function ProfileTest() {
 
 const styles = {
   bg: {
-    height: height,
+    flex: 1,
     backgroundColor: colors.gray_bg,
   },
   toptab: {
     backgroundColor: colors.pink,
     textAlign: "center",
-    height: "7.325%",
+    height: height * 0.09625,
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "center",
     paddingHorizontal: width * 0.1,
-    paddingTop: height * 0.025,
-    marginTop: height * 0.025,
+    marginTop: 30,
     position: "relative",
   },
   usernameContainer: {
@@ -276,7 +275,6 @@ const styles = {
   signoutContainer: {
     position: "absolute",
     right: 20,
-    paddingTop: height * 0.025,
   },
   pencilContainer: {
     bottom: -2,
